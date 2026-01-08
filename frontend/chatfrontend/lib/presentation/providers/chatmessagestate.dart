d7b4280db9b54482cb2ab60b811458f5a95af056
Umanga <umanga.gurung63@gmail.com>
2026-01-08 14:58:04 +0545

    Implemented event queue to avoid race conditions on message callback

--- frontend/chatfrontend/lib/presentation/providers/chatmessagestate.dart
+++ frontend/chatfrontend/lib/presentation/providers/chatmessagestate.dart
@@ -1,32 +1,48 @@
+import 'dart:async';
+
 import 'package:chatfrontend/cache/service/hivemessageservice.dart';
+import 'package:chatfrontend/dto/message/messageresponsedto.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import '../../dto/message/messagedetailsdto.dart';
 
 class ChatMessageState extends Notifier<Map<String, List<MessageDetailsDTO>>> {
+  late final HiveMessageService _hiveMessageService;
+  late final StreamController<MessageResponseDTO> _queue;
+
   @override
   Map<String, List<MessageDetailsDTO>> build() {
     // TODO: implement build
+    _hiveMessageService = HiveMessageService();
+
+    _queue= StreamController<MessageResponseDTO>();
+    _queue.stream.asyncMap((msg) async {
+      final conversationId= msg.conversationId;
+
+      if(await _hiveMessageService.isExpired(conversationId)){
+        await _hiveMessageService.setExpirationTime(conversationId);
+      }
+
+      await _hiveMessageService.addMessageToHive(msg, conversationId);
+    }).listen(null);
+
     return {};
   }
 
-  // HiveMessageService _hiveMessageService= HiveMessageService();
+  void addNewMessages(MessageDetailsDTO message) async {
+    final conversationId = message.messageResponseDTO.conversationId;
 
- void addNewMessages(MessageDetailsDTO message){
-    final convoId = message.messageResponseDTO.conversationId;
-
-    final existingMessages = state[convoId] ?? const [];
+    final existingMessages = state[conversationId] ?? const [];
 
     state = {
       ...state,
-      convoId: [message, ...existingMessages],
+      conversationId: [message, ...existingMessages],
     };
+
+    _queue.add(message.messageResponseDTO);
   }
 
-  void clearState(String convoId){
-    state = {
-      ...state,
-      convoId: [],
-    };
+  void clearState(String conversationId) {
+    state = {...state, conversationId: []};
   }
 }
